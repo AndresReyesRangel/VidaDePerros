bc431f03f6552c9696758a017572455c10031b0f
zambranoconz <riczam2797@gmail.com>
2020-03-29 20:39:54 -0600

    Actualizacion botón "Acerca De"

---
+++ core/src/mx/itesm/proyecto/PantallaAcercaDe.java
@@ -0,0 +1,33 @@
+package mx.itesm.proyecto;
+
+import com.badlogic.gdx.Screen;
+
+class PantallaAcercaDe extends Pantalla {
+    public PantallaAcercaDe(Juego juego) {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- core/src/mx/itesm/proyecto/PantallaMenu.java
+++ core/src/mx/itesm/proyecto/PantallaMenu.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -24,24 +25,36 @@ class PantallaMenu extends Pantalla {
 
     @Override
     public void show() {
-        texturaFondo = new Texture("Menu/fondoMenu.png");
+       // texturaFondo = new Texture("Menu/fondoMenu.png");
         crearMenu();
     }
 
     private void crearMenu() {
         escenaMenu = new Stage(vista);
 
-        //Prueba botones
+        //Botones cargando imagenes
+
 
         Texture texturaBtnJugar = new Texture("Menu/btnJugar.png");
         TextureRegionDrawable trdJugar = new TextureRegionDrawable(new TextureRegion(texturaBtnJugar));
+
         //Imagen btn presionado
         Texture texturaBtnJugarP = new Texture("Menu/btnJugarP.png");
         TextureRegionDrawable trdJugarP = new TextureRegionDrawable(new TextureRegion(texturaBtnJugarP));
 
+        Texture texturaBtnAcercaDe = new Texture("Menu/btnAcercaDe.png");
+        TextureRegionDrawable trdAcercaDe = new TextureRegionDrawable(new TextureRegion(texturaBtnAcercaDe));
+
+        Texture texturaBtnAcercaDeP = new Texture("Menu/btnAcercaDeP.png");
+        TextureRegionDrawable trdAcercaDeP = new TextureRegionDrawable(new TextureRegion(texturaBtnAcercaDeP));
+
+        //Cargando los botones
+
         ImageButton btnJugar = new ImageButton(trdJugar, trdJugarP);
+        ImageButton btnAcercaDe = new ImageButton(trdAcercaDe, trdAcercaDeP);
 
         btnJugar.setPosition(ANCHO/2-btnJugar.getWidth()/2 , 2*ALTO/3 );
+        btnAcercaDe.setPosition(ANCHO/2-btnJugar.getWidth()/2 - 60 , ALTO/3);
 
         //Listener
         btnJugar.addListener(new ClickListener(){
@@ -52,7 +65,16 @@ class PantallaMenu extends Pantalla {
             }
         });
 
+        btnAcercaDe.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                juego.setScreen(new PantallaAcercaDe(juego));
+            }
+        });
+
         escenaMenu.addActor(btnJugar);
+        escenaMenu.addActor(btnAcercaDe);
 
         Gdx.input.setInputProcessor(escenaMenu);
     }
@@ -63,7 +85,7 @@ class PantallaMenu extends Pantalla {
         batch.setProjectionMatrix(camara.combined);
 
         batch.begin();
-        batch.draw(texturaFondo,0,0);
+        //batch.draw(texturaFondo,0,0);
         batch.end();
 
         escenaMenu.draw();

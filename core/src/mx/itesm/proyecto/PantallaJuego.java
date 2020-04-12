bbd1a24376739b3fce7c59507325dcfac892f6a3
Andrés Reyes <A01746592@itesm.mx>
2020-04-11 19:48:36 -0500

    Fondo casi infinito

--- core/src/mx/itesm/proyecto/PantallaJuego.java
+++ core/src/mx/itesm/proyecto/PantallaJuego.java
@@ -9,6 +9,7 @@ class PantallaJuego extends Pantalla {
 
     //Fondo
     private Texture texturaFondo;
+    private Fondo fondo;
 
     public PantallaJuego(Juego juego) {
         this.juego = juego;
@@ -17,17 +18,33 @@ class PantallaJuego extends Pantalla {
     @Override
     public void show() {
         texturaFondo = new Texture("PantallaJuego/FondoJuego.png");
+        crearFondo();
     }
 
     @Override
     public void render(float delta) {
         borrarPantalla();
+        actualizar(delta);
         batch.setProjectionMatrix(camara.combined);
-
         batch.begin();
-        batch.draw(texturaFondo,0,0);
+
+
+        fondo.render(batch);
+
         batch.end();
 
+    }
+
+    private void actualizar(float delta) {
+        moverFondo(delta);
+    }
+
+    public void crearFondo(){
+        fondo = new Fondo(texturaFondo, 0, 0);
+    }
+
+    private void moverFondo(float delta) {
+        fondo.mover(delta);
     }
 
     @Override
